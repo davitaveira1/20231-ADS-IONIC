0dfcb0c77eca4c1b255b821e18e75cffec499d90
Davi Taveira <davitaveira@gmail.com>
2023-03-03 21:07:08 -0300

    botao

--- app-0303/src/app/app-routing.module.ts
+++ app-0303/src/app/app-routing.module.ts
@@ -11,6 +11,18 @@ const routes: Routes = [
     path: 'pagprincipal',
     loadChildren: () => import('./pagprincipal/pagprincipal.module').then( m => m.PagprincipalPageModule)
   },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./ex01/inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'pag1',
+    loadChildren: () => import('./ex01/pag1/pag1.module').then( m => m.Pag1PageModule)
+  },
+  {
+    path: 'pag2',
+    loadChildren: () => import('./ex01/pag2/pag2.module').then( m => m.Pag2PageModule)
+  },
 ];
 
 @NgModule({
